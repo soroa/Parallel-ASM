ad69ed37564e230ebbe06584ed4909bec943b3fe
soroa <soroa@student.ethz.ch>
2016-11-06 19:07:25 +0100

    fixed bug memory efficient

--- src/parallel/parallelASM.c
+++ src/parallel/parallelASM.c
@@ -27,6 +27,13 @@ void printM(int** M,int m){
     printf("\n");
   }
 }
+  
+
+int mod(int a, int b)
+{
+    int r = a % b;
+    return r < 0 ? r + b : r;
+}
 
 
 int main (int argc, char *argv[]) 
@@ -60,51 +67,59 @@ int main (int argc, char *argv[])
   }
 
  
-  printM(DMatrix, m);
+  // printM(DMatrix, m);
 
 
 
   int minVal = m; 
-
+  int j=0;
+  DMatrix[0][0]=0;
   for(int k=0; k<=n+m-1; k++){
     printf("k : %d \n", k);
-    int j=0;
     for(int i=0; i<=m; i++){
 
       j = k-i+1; 
   
-      printf("i is %d and j is %d \n", i, j);
+      // printf("i is %d and j is %d \n", i, j);
       if(i==0){
         DMatrix[j%3][i]=0; 
-        continue;
+        printf("set %d %d %d\n", j, i,   DMatrix[j%3][i]);
+
       }
 
-      else if(j=0) {
+      else if(j==0) {
         DMatrix[j%3][i] = i; 
-        continue;
+        printf("set %d %d %d\n", j, i,   DMatrix[j%3][i]);
+
       }
       
-      else if(j>=1 && j<=n){
+      else if((j>=1) && (j<=n)) {
         
-        printf("assignment loop \n"); 
-        DMatrix[j%3][i] = MIN(DMatrix[(j-1)%3][i]+1, MIN(DMatrix[(j)%3][i-1]+1, DMatrix[(j-1)%3][i-1]+1)+ (pattern[i]==targetText[j]?1:0));
+        // printf("assignment loop \n"); 
+        DMatrix[j%3][i] = MIN(DMatrix[mod((j-1),3)][i]+1, MIN(DMatrix[mod((j),3)][i-1]+1, DMatrix[mod((j-1),3)][i-1]+ (pattern[i-1]==targetText[j-1]?0:1)));
+        printf("check %d %d %d\n", DMatrix[mod((j-1),3)][i]+1,DMatrix[mod((j),3)][i-1]+1,DMatrix[mod((j-1),3)][i-1]+ (pattern[i-1]==targetText[j-1]?0:1)) ;
+        // printf(" check prev diag elem %d, chars are disequal: %d", DMatrix[mod((j-1),3)][i-1], (pattern[i-1]==targetText[j-1]?0:1));
+        printf("set %d %d %d\n", j, i,   DMatrix[j%3][i]);
+        // printM(DMatrix,m);
+        if(i==m){
+        if(DMatrix[mod(j,3)][m]<minVal) minVal = DMatrix[mod(j,3)][m]; 
+        }
       }
 
-      if(i==m){
-      if(DMatrix[j%3][i]<minVal) minVal = DMatrix[j%3][m]; 
-      }
+     }  
 
-
+    
+    printM(DMatrix,m);
     }
     
 
-    printf("\n");
+
   
 
-    printf("\n \n Min value is %d \n \n ", minVal); 
-
+   printf("\n \n Min value is %d \n \n ", minVal); 
+   free(DMatrix);
   }
-  free(DMatrix);
 
 
-}
+
+
