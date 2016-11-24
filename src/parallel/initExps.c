cb568a7ee218159e2c3ac542e13d8e96cf12038c
soroa <soroa@student.ethz.ch>
2016-11-24 17:47:50 +0100

    implemented baseline2 with openMP row-wise C Matrix Computation

---
+++ src/parallel/initExps.c
@@ -0,0 +1,65 @@
+// Dynamic Programming - C table : k+1 threads, O(n) in theory
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include <pthread.h>
+#include <omp.h>
+
+
+double *C;
+static const int NUMBER_OF_THREADS = 4;
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+    int size = 100000000;
+    C = (double *)malloc(size * sizeof(double*));
+    //SEQUENTIAL VERSION
+    // for(int i =0; i<size; i=i+1){
+    //         C[i]=sqrt(i);
+    //     }
+
+
+
+
+    static const int NUMBER_OF_THREADS = 4;
+    #pragma omp parallel
+    {
+        int nthreads;
+        nthreads = omp_get_num_threads();
+
+        int ID = omp_get_thread_num();
+        //PARALLEL VERSION 1: P1|P2|P3|P4|P1|P2|.... 
+        // for (int i = ID; i < size; i = i + nthreads) {
+        //     C[i] = sqrt(i);
+        // }
+
+        //PARALLEL VERSION 2: |...P1...|....P2....|....P3.....|....P4.....|
+        int subportion  = (size) / nthreads;
+        int remainder = (size) % nthreads;
+        if (ID == 0) {
+            for (int i = 0; i < subportion + remainder; i++)
+                C[i] = C[i] = sqrt(i);
+        } else {
+            for (int i = ID * subportion + remainder; i < ID * subportion + subportion + remainder; i++)
+                C[i] = C[i] = sqrt(i);
+        }
+
+
+
+    }
+
+
+    free(C);
+
+
+
+
+}
--- src/parallel/parallel_baseline2openMP.c
+++ src/parallel/parallel_baseline2openMP.c
@@ -54,23 +54,18 @@ int main(int argc, char *argv[])
     for (int i = 0; i < k + 2; i++)
         C[i] = (int *)malloc((n - m + 2 * k + 3) * sizeof(int));
 
-    // for (int i = 0; i < k + 2; i++){
-    //     for (int j = 0; j < n - m + 2 * k + 3; j++){
+    // for (int i = 0; i < k + 2; i++) {
+    //     for (int j = 0; j < n - m + 2 * k + 3; j++) {
 
     //         C[i][j] = not_initialized;
 
     //     }
     // }
-    //  printC();
 
 
-    // for (int d = 0; d <= (n - m + k + 1); d++)
-    //     set_C_table(-1, d, d - 1);
-    // for (int d = -(k + 1); d <= -1; d++) {
-    //     set_C_table(-d - 1, d, -1);
-    //     set_C_table(-d - 2, d, INT_MIN);
-    // }
-    // printC();
+
+
+
 
     printf("C allocated \n ");
 
@@ -89,43 +84,44 @@ int main(int argc, char *argv[])
         /*
         *   D MATRIX INITIALIZATION
         */
-        if (ID == 0) {
-            printf("initialization started\n");
-        }
-        //one thread per row
+
+    
+
         for (int i = ID; i < k + 2; i = i + nthreads)
-            for (int j = 0; j < n - m + 2 * k + 3; j++)
+           for (int j = 0; j < n-m+2*k+3; j++)
                 C[i][j] = not_initialized;
-        // initializing first row. Splitting the row in equal parts. Thread 0 taking care of remainder
-        // int subportion  = (n - m + k + 2) / nthreads;
-        // int remainder = (n - m + k + 2) % nthreads;
-        // if (ID == 0) {
-        //     for (int d = 0; d < subportion + remainder; d++)
-        //         set_C_table(-1, d, d - 1);
-        // } else {
-        //     for (int d = ID * subportion + remainder; d < ID * subportion + subportion + remainder; d++)
-        //         set_C_table(-1, d, d - 1);
-        // }
-        // if (ID == 0) {
-        //     printf("initialization in progress \n");
-        // }
-
-        //initializng two diagonals to -infinity and -1. One thread per row
-     
 
 
+        #pragma omp barrier 
+
+        int subportion  = (n - m + k + 2) / nthreads;
+        int remainder = (n - m + k + 2) % nthreads;
         if (ID == 0) {
-            for (int d = 0; d <= (n-m+k+1); d++)
-            set_C_table(-1, d, d-1);
-            for (int d = -(k + 1); d <= -1; d = d + 1) {
-                set_C_table(-d - 1, d, -1);
-                set_C_table(-d - 2, d, INT_MIN);
-            }
-            printf("initialization completed\n");
-            printC();
+            for (int d = 0; d < subportion + remainder; d++)
+                set_C_table(-1, d, d - 1);
+        } else {
+            for (int d = ID * subportion + remainder; d < ID * subportion + subportion + remainder; d++)
+                set_C_table(-1, d, d - 1);
         }
+    
+
+        for (int d = -(k + 1) + ID; d <= -1; d = d + nthreads) {
+            set_C_table(-d - 1, d, -1);
+            set_C_table(-d - 2, d, INT_MIN);
+        }
+
+
+        //**********************************************************
+        //************************  Computation  *********************
+        //**********************************************************
+
+
+
+    
         #pragma omp barrier
-        for (int e = ID; e <= 0; e = e + nthreads)
+
+
+        for (int e = ID; e <= k; e = e + nthreads)
         {
             for (int c = 0; c <= n - m + k; c++)
             {
@@ -142,20 +138,33 @@ int main(int argc, char *argv[])
             }
         }
 
+
+        //**********************************************************
+        //************************  Output: working in parallel  *********************
+        //**********************************************************
+
+
         #pragma omp barrier
-        // //Matrix computed. Going through last line
-        // printf("result");
-        // for (int d = -k + ID; d <= n - m; d = d + nthreads)
-        //     if (get_C_table(k, d) == d + m && d + m > 0)
-        //         printf("%d ", d + m);
+        //Matrix computed. Going through last line
+
+
+        for (int d = -k + ID ; d <= n - m; d = d + nthreads)
+            if (get_C_table(k, d) == d + m && d + m > 0)
+                printf("%d  ", d + m);
 
 
 
+        //**********************************************************
+        //************************  Dealloc: working in parallel  *********************
+        //**********************************************************
+
+        #pragma omp barrier
+        for (int i = ID; i < k + 2; i = i + nthreads)
+            free(C[i]);
 
     }
-    printC(); 
-    for (int i = 0; i < k + 2; i++)
-        free(C[i]);
+
+
     free(C);
 
 
