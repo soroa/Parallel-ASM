0f128882544b86b527fce78d2513bf387c7edd43
soroa <soroa@student.ethz.ch>
2016-12-09 20:34:57 +0100

    done parallel bl 2 omp row with faster initialization

--- src/all/par_1_omp_r.c
+++ src/all/par_1_omp_r.c
@@ -10,7 +10,7 @@
 int n, m, k;
 char  *pattern;
 int **D;
-short *initializedLines; 
+short *initializedLines;
 
 char *text, *pattern;
 static const int NUMBER_OF_THREADS = 8;
@@ -28,12 +28,12 @@ void printD(int** D) {
 
 }
 
-void readTextandPattern(char *argv[]){
-	char *textFileName; 
-	char *patternFileName; 
+void readTextandPattern(char *argv[]) {
+	char *textFileName;
+	char *patternFileName;
 	textFileName = argv[1];
 	patternFileName = argv[2];
-	//reading from text file 
+	//reading from text file
 	FILE *f = fopen(textFileName, "r");
 	if (f == NULL)
 	{
@@ -48,7 +48,7 @@ void readTextandPattern(char *argv[]){
 	char textBuf[SIZE + 1];
 	if (fgets( textBuf, SIZE + 1, f) != NULL) {
 		// printf("text read correctly\n");
-		text = textBuf; 
+		text = textBuf;
 	} else {
 		// printf("returned null \n");
 	}
@@ -77,8 +77,8 @@ void readTextandPattern(char *argv[]){
 
 int main(int argc, char *argv[]) {
 	unsigned long long t1, t2;
-	
-	
+
+
 
 	if (argc != 4) {
 
@@ -87,7 +87,7 @@ int main(int argc, char *argv[]) {
 	}
 
 
-	readTextandPattern(argv); 
+	readTextandPattern(argv);
 
 	n = strlen(text);
 	m = strlen(pattern);
@@ -98,13 +98,13 @@ int main(int argc, char *argv[]) {
 	*	ALLOCATING MEMORY FOR D, (N+1xM+1)
 	*/
 
-	t1=rdtsc();
+	t1 = rdtsc();
 	D = (int **)malloc((m + 1) * sizeof(int *));
 	for (int i = 0; i < m + 1; i = i + 1) {
 		D[i] = (int *)malloc((n + 1) * sizeof(int));
 	}
-	
-	initializedLines = (short *) calloc((m+1),sizeof(short)); 
+
+	initializedLines = (short *) calloc((m + 1), sizeof(short));
 
 
 	/*
@@ -123,37 +123,39 @@ int main(int argc, char *argv[]) {
 		*	D MATRIX INITIALIZATION
 		*/
 
-		
-        if (ID == 0) {
-            for (int i = 0; i <= n; i = i + 1)
-                D[0][i] = 0;
-            initializedLines[0]=1; 
 
-        }
+		if (ID == 0) {
+			for (int i = 0; i <= n; i = i + 1)
+				D[0][i] = 0;
+			initializedLines[0] = 1;
 
-        if (ID == 1) {
-            // printf("initializing first colum\n");
-            for (int i = 1 ; i <= m; i = i + 1)
-                D[i][0] = i;
-        }
+		}
 
-        if(ID>=nthreads/2){
-        for (int i = 1 + ID - nthreads/2; i <= m; i = i + nthreads/2)
-            for (int j = 1; j <= n; j = j + 1) {
-                D[i][j] = -1;
-                initializedLines[i]=1;
-            }
-        }
+		if (ID == 1) {
+			// printf("initializing first colum\n");
+			for (int i = 1 ; i <= m; i = i + 1)
+				D[i][0] = i;
+		}
+
+		if (ID >= nthreads / 2) {
+			for (int i = 1 + ID - nthreads / 2; i <= m; i = i + nthreads / 2) {
+				for (int j = 1; j <= n; j = j + 1) {
+					D[i][j] = -1;
+
+				}
+				initializedLines[i] = 1;
+			}
+		}
 		//threads must sync here
 
 		/*
 		*	D MATRIX COMPUTATION
 		*/
-		if(ID<nthreads/2){
+		if (ID < nthreads / 2) {
 
-			for (int i = ID + 1; i <= m; i = i + nthreads/2 ) {
-			 	while(initializedLines[i]==0){
-            	}
+			for (int i = ID + 1; i <= m; i = i + nthreads / 2 ) {
+				while (initializedLines[i] == 0) {
+				}
 				for (int j = 1; j <= n; j++) {
 					while (D[i - 1][j] == -1) {}
 					D[i][j] = fmin(fmin(D[i - 1][j] + 1, D[i][j - 1] + 1), D[i - 1][j - 1] + (pattern[i - 1] == text[j - 1] ? 0 : 1));
@@ -171,7 +173,7 @@ int main(int argc, char *argv[]) {
 
 		for (int i = ID; i <= n; i = i + nthreads) {
 			// if (D[m][i] <= k)
-				// printf("k-match at i:%d \n", i)		
+			// printf("k-match at i:%d \n", i)
 		}
 
 
@@ -188,8 +190,8 @@ int main(int argc, char *argv[]) {
 
 	free(D[m]);
 	free(D);
- 	t2=rdtsc();		
-	printf("%llu \n", t2-t1);
+	t2 = rdtsc();
+	printf("%llu \n", t2 - t1);
 
 
 
--- src/all/par_2_omp_r.c
+++ src/all/par_2_omp_r.c
@@ -11,18 +11,19 @@
 int n, m, k;
 char *text, *pattern;
 int **C;
-static const int NUMBER_OF_THREADS = 4;
+static const int NUMBER_OF_THREADS = 8;
+short *initializedLines;
 
 
 void set_C_table(int i, int j, int value)
 {
 
-    C[i + 1][j + k + 1] = value;
+  C[i + 1][j + k + 1] = value;
 }
 
 int get_C_table(int i, int j)
 {
-    return C[i + 1][j + k + 1];
+  return C[i + 1][j + k + 1];
 }
 
 
@@ -30,13 +31,13 @@ int get_C_table(int i, int j)
 
 void printC() {
 
-    for (int j = 0; j < k + 2; j++) {
-        for (int i = 0; i < n - m + 2 * k + 3; i++) {
-            printf("|%d| ", C[j][i]);
-        }
-        printf("\n");
+  for (int j = 0; j < k + 2; j++) {
+    for (int i = 0; i < n - m + 2 * k + 3; i++) {
+      printf("|%d| ", C[j][i]);
     }
-    printf("\n\n\n");
+    printf("\n");
+  }
+  printf("\n\n\n");
 
 
 }
@@ -90,128 +91,157 @@ void readTextandPattern(char *argv[]) {
 
 int main(int argc, char *argv[])
 {
-	unsigned long long t1, t2;
-	t1=rdtsc();
-    if (argc != 4) {
-        printf("\n usage: ./exec text pattern k \n \n ");
-        return 0;
-    }
-   readTextandPattern(argv);
-    n = strlen(text);
-    m = strlen(pattern);
-    k = atoi(argv[3]);
+
+
+
+
+
+  unsigned long long t1, t2;
+  t1 = rdtsc();
+  if (argc != 4) {
+    printf("\n usage: ./exec text pattern k \n \n ");
+    return 0;
+  }
+  readTextandPattern(argv);
+
+
+  n = strlen(text);
+  m = strlen(pattern);
+  k = atoi(argv[3]);
+  int not_initialized = -2;
+
+  C = (int **)malloc((k + 2) * sizeof(int *));
+  for (int i = 0; i < k + 2; i++)
+    C[i] = (int *)malloc((n - m + 2 * k + 3) * sizeof(int));
+
+  initializedLines = (short *) calloc((k + 2), sizeof(short));
+
+  // printf("C allocated \n ");
+
+
+
+
+  #pragma omp parallel
+  {
+    int nthreads;
+    nthreads = omp_get_num_threads();
+
+    int ID = omp_get_thread_num();
+
     int not_initialized = -2;
 
-    C = (int **)malloc((k + 2) * sizeof(int *));
-    for (int i = 0; i < k + 2; i++)
-        C[i] = (int *)malloc((n - m + 2 * k + 3) * sizeof(int));
 
 
-    // printf("C allocated \n ");
-
-
-
-    static const int NUMBER_OF_THREADS = 4;
-    #pragma omp parallel
-    {
-        int nthreads;
-        nthreads = omp_get_num_threads();
-
-        int ID = omp_get_thread_num();
-
-        int not_initialized = -2;
-
-
-
-        //**********************************************************
-        //************************  MATRIX INITIALIZATION  *********************
-        //**********************************************************
+    //**********************************************************
+    //************************  MATRIX INITIALIZATION  *********************
+    //**********************************************************
 
 
 
 
-        for (int i = ID; i < k + 2; i = i + nthreads)
-            for (int j = 0; j < n - m + 2 * k + 3; j++)
-                C[i][j] = not_initialized;
+
+  
 
 
-        #pragma omp barrier
+    #pragma omp barrier
 
-        int subportion  = (n - m + k + 2) / nthreads;
-        int remainder = (n - m + k + 2) % nthreads;
-        if (ID == 0) {
-            for (int d = 0; d < subportion + remainder; d++)
-                set_C_table(-1, d, d - 1);
-        } else {
-            for (int d = ID * subportion + remainder; d < ID * subportion + subportion + remainder; d++)
-                set_C_table(-1, d, d - 1);
+    int subportion  = (n - m + k + 2) / nthreads;
+    int remainder = (n - m + k + 2) % nthreads;
+
+
+
+
+//first line initialize
+
+    if (ID == 0) {
+      for (int d = 0; d < subportion + remainder; d++)
+        set_C_table(-1, d, d - 1);
+    } else {
+      for (int d = ID * subportion + remainder; d < ID * subportion + subportion + remainder; d++)
+        set_C_table(-1, d, d - 1);
+    }
+
+    initializedLines[0] = 1;
+
+
+// //first two diagonals initialized
+
+    for (int d = -(k + 1) + ID; d <= -1; d = d + nthreads) {
+      set_C_table(-d - 1, d, -1);
+      set_C_table(-d - 2, d, INT_MIN);
+    }
+
+    if (ID >= nthreads / 2) {
+      for (int e = ID - nthreads / 2; e <= k; e = e + nthreads / 2) {
+        for (int c = 0; c <= n - m + k; c++) {
+          set_C_table(e, c - e, not_initialized);
         }
+        initializedLines[e+1] = 1;
 
-
-        for (int d = -(k + 1) + ID; d <= -1; d = d + nthreads) {
-            set_C_table(-d - 1, d, -1);
-            set_C_table(-d - 2, d, INT_MIN);
-        }
-
-
-        //**********************************************************
-        //************************  Computation: working in parallel  *********************
-        //**********************************************************
-
-
-
-
-        #pragma omp barrier
-
-
-        for (int e = ID; e <= k; e = e + nthreads)
-        {
-            for (int c = 0; c <= n - m + k; c++)
-            {
-                int d = c - e;
-                while (get_C_table(e - 1, d + 1) == not_initialized) {
-                    // printf("%d waiting \n", ID);
-                    // printf("wait C[%d, %d]\n", e-1, d+1);
-                }
-                int col = fmax(fmax(get_C_table(e - 1, d - 1) + 1, get_C_table(e - 1, d) + 1), get_C_table(e - 1, d + 1));
-                while (col < n && col - d < m && text[col] == pattern[col - d]) {
-                    col++;
-                }
-                set_C_table(e, d, fmin(fmin(col, m + d), n));
-            }
-        }
-
-
-        //**********************************************************
-        //************************  Output: working in parallel  *********************
-        //**********************************************************
-
-
-        #pragma omp barrier
-        //Matrix computed. Going through last line
-
-
-        for (int d = -k + ID ; d <= n - m; d = d + nthreads)
-            // if (get_C_table(k, d) == d + m && d + m > 0)
-                // printf("%d  ", d + m);
-
-
-
-        //**********************************************************
-        //************************  Dealloc: working in parallel  *********************
-        //**********************************************************
-
-        #pragma omp barrier
-        for (int i = ID; i < k + 2; i = i + nthreads)
-            free(C[i]);
+      }
 
     }
 
 
-    free(C);
+//     //**********************************************************
+//     //************************  Computation: working in parallel  *********************
+//     //**********************************************************
 
-    t2=rdtsc();
-	printf("%llu \n", t2-t1);
+
+
+
+    if (ID < nthreads / 2) {
+
+      for (int e = ID; e <= k; e = e + nthreads / 2)
+      {
+        while (initializedLines[e+1] == 0) {
+          // printf("T %d waiting for line %d \n", ID, e - 1);
+        }
+        for (int c = 0; c <= n - m + k; c++)
+        {
+          int d = c - e;
+          while (get_C_table(e - 1, d + 1) == not_initialized) {}
+          int col = fmax(fmax(get_C_table(e - 1, d - 1) + 1, get_C_table(e - 1, d) + 1), get_C_table(e - 1, d + 1));
+          while (col < n && col - d < m && text[col] == pattern[col - d]) {
+            col++;
+          }
+          set_C_table(e, d, fmin(fmin(col, m + d), n));
+        }
+      }
+    }
+
+
+//     //**********************************************************
+//     //************************  Output: working in parallel  *********************
+//     //**********************************************************
+
+
+     #pragma omp barrier
+//     //Matrix computed. Going through last line
+
+
+//     // for (int d = -k + ID ; d <= n - m; d = d + nthreads)
+//       // if (get_C_table(k, d) == d + m && d + m > 0)
+//       // printf("%d  ", d + m);
+
+
+
+//       //**********************************************************
+//       //************************  Dealloc: working in parallel  *********************
+//       //**********************************************************
+
+      #pragma omp barrier
+      for (int i = ID; i < k + 2; i = i + nthreads)
+        free(C[i]);
+
+  }
+
+
+  
+  free(C);
+
+  t2 = rdtsc();
+  printf("%llu \n", t2 - t1);
 
 
 }
